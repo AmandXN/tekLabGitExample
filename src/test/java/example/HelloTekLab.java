50a5e4db211822670fd5fd1b35c9c5bbd99b6fe1
gulza <gulza@DESKTOP-LFVUM6A>
2020-10-31 10:04:41 -0500

    added a good method in HelloTekleb

--- src/test/java/example/HelloTekLab.java
+++ src/test/java/example/HelloTekLab.java
@@ -8,14 +8,19 @@ public class HelloTekLab {
 	public static void main(String[] args) {
 
 		System.out.println("Hello TekLab Students");
+		System.out.println("This is my change");
 		
-
+		
+	
 	}
 	
 	@BeforeMethod
 	public void setUp() {
 		String driverpath = System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", driverpath);
+	}
+	public void goodMethod() {
+		System.out.println("this is new method");
 	}
 
 }
